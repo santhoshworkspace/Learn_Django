581f75300fcc6c703ee08a11f099f9d1ecdc9b95
santhoshworkspace.gmail.com <santhoshworkspace@gmail.com>
2025-06-02 11:03:57 +0530

    Day 2

--- myapp/blog/urls.py
+++ myapp/blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
-    path("",views.index,name="index"),
-    path("new",views.new,name="new"),
+    path("",views.base,name="base"),
+    path("post",views.detail,name="detail"),
+    path("new",views.new,name="new_path"),
     path("old_url",views.old_url,name="old_url"),
-    path("new_url",views.new_url,name="new_url")
+    path("new_url",views.new_url,name="new_url_blog"),
+    path("index",views.index,name="index"),
+    path("*",views.all,name="All")
 ]
--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.urls import reverse
 # Create your views here.
 def index (request):
-    return HttpResponse("Hello ")
+    return render(request,'index.html')
 
 def new (request):
-    return HttpResponse("New")
+    return HttpResponse("centeral bank")
 
 def old_url(request):
-    return redirect("new_url")
+    return redirect(reverse("blog:new_url_blog"))
 
 def new_url(request):
     return HttpResponse("new url")
+
+def all(request):
+    return HttpResponse("*")
+
+def detail(request):
+    return render(request,'detail.html')
+
+def base(request):
+    return render(request,'base.html')
